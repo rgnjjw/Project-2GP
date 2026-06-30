36668a0f05dd7641339d599334a0906cdcbcdc0e
rgnjjw <rgn7jjw@gmail.com>
2026-06-30 14:57:46 +0900

    d

--- Assets/02 Scripts/Enemy/HitFlash.cs
+++ Assets/02 Scripts/Enemy/HitFlash.cs
@@ -11,30 +11,48 @@ namespace _02_Scripts.Enemy
 
         private SkinnedMeshRenderer[] _renderers;
         private MaterialPropertyBlock _mpb;
+        private Color[] _originalEmissions;
         private static readonly int EmissionColor = Shader.PropertyToID("_EmissionColor");
+
+        private Tween _activeTween;
 
         public void Initialize(ModuleOwner owner)
         {
             _renderers = owner.GetComponentsInChildren<SkinnedMeshRenderer>();
             _mpb = new MaterialPropertyBlock();
+            _originalEmissions = new Color[_renderers.Length];
+
+            for (int i = 0; i < _renderers.Length; i++)
+            {
+                if (_renderers[i].sharedMaterial.HasProperty(EmissionColor))
+                {
+                    _originalEmissions[i] = _renderers[i].sharedMaterial.GetColor(EmissionColor);
+                }
+                else
+                {
+                    _originalEmissions[i] = Color.black;
+                }
+            }
         }
 
         public void Flash()
         {
-            foreach (var r in _renderers)
+            _activeTween?.Kill();
+
+            for (int i = 0; i < _renderers.Length; i++)
             {
-                r.GetPropertyBlock(_mpb);
+                _renderers[i].GetPropertyBlock(_mpb);
                 _mpb.SetColor(EmissionColor, flashColor);
-                r.SetPropertyBlock(_mpb);
+                _renderers[i].SetPropertyBlock(_mpb);
             }
 
-            DOVirtual.DelayedCall(flashDuration, () =>
+            _activeTween = DOVirtual.DelayedCall(flashDuration, () =>
             {
-                foreach (var r in _renderers)
+                for (int i = 0; i < _renderers.Length; i++)
                 {
-                    r.GetPropertyBlock(_mpb);
-                    _mpb.SetColor(EmissionColor, Color.black);
-                    r.SetPropertyBlock(_mpb);
+                    _renderers[i].GetPropertyBlock(_mpb);
+                    _mpb.SetColor(EmissionColor, _originalEmissions[i]);
+                    _renderers[i].SetPropertyBlock(_mpb);
                 }
             });
         }
