622b495bc6cac618c6337fdf21b8b42dde8f105c
rgnjjw <rgn7jjw@gmail.com>
2026-07-02 14:25:42 +0900

    ㅇ

--- Assets/02 Scripts/UI/UIHoverScale.cs
+++ Assets/02 Scripts/UI/UIHoverScale.cs
@@ -2,6 +2,7 @@ using csiimnida.CSILib.SoundManager.RunTime;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace _02_Scripts.UI
 {
@@ -11,31 +12,59 @@ namespace _02_Scripts.UI
         [SerializeField] private float duration = 0.2f;
         [SerializeField] private Ease ease = Ease.OutBack;
 
+        [Header("Optional Hover Image")]
+        [SerializeField] private Image hoverImage;
+
         private Vector3 _originalScale;
-        private Tween _tween;
+        private Vector3 _originalImageScale;
+        private Tween _scaleTween;
+        private Tween _imageScaleTween;
 
         private void Awake()
         {
             _originalScale = transform.localScale;
+
+            if (hoverImage != null)
+            {
+                _originalImageScale = hoverImage.transform.localScale;
+            }
         }
 
         public void OnPointerEnter(PointerEventData eventData)
         {
-            _tween?.Kill();
-            _tween = transform.DOScale(_originalScale * hoverScale, duration).SetEase(ease).SetUpdate(true);
+            _scaleTween?.Kill();
+            _scaleTween = transform.DOScale(_originalScale * hoverScale, duration).SetEase(ease).SetUpdate(true);
             SoundManager.Instance.PlaySound("UIHover");
+
+            if (hoverImage != null)
+            {
+                _imageScaleTween?.Kill();
+                _imageScaleTween = hoverImage.transform.DOScale(_originalImageScale * hoverScale, duration).SetEase(ease).SetUpdate(true);
+            }
         }
 
         public void OnPointerExit(PointerEventData eventData)
         {
-            _tween?.Kill();
-            _tween = transform.DOScale(_originalScale, duration).SetEase(ease).SetUpdate(true);
+            _scaleTween?.Kill();
+            _scaleTween = transform.DOScale(_originalScale, duration).SetEase(ease).SetUpdate(true);
+
+            if (hoverImage != null)
+            {
+                _imageScaleTween?.Kill();
+                _imageScaleTween = hoverImage.transform.DOScale(_originalImageScale, duration).SetEase(ease).SetUpdate(true);
+            }
         }
 
         private void OnDisable()
         {
-            _tween?.Kill();
+            _scaleTween?.Kill();
+            _imageScaleTween?.Kill();
             transform.localScale = _originalScale;
+
+            if (hoverImage != null)
+            {
+                hoverImage.transform.localScale = _originalImageScale;
+            }
         }
     }
 }
